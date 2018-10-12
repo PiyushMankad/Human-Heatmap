c4c5ea0b002383e29bdbafaf3bd5186370df65a3
PiyushMankad <32595688+PiyushMankad@users.noreply.github.com>
2018-10-12 11:37:29 +0530

    Add files via upload

--- humanDetect.py
+++ humanDetect.py
@@ -16,7 +16,7 @@ ap.add_argument("-p", "--prototxt", default="MobileNetSSD_deploy.prototxt.txt",
 	help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", default="MobileNetSSD_deploy.caffemodel",
 	help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.8,
+ap.add_argument("-c", "--confidence", type=float, default=0.02,
 	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
@@ -36,80 +36,57 @@ net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 # initialize the video stream, allow the cammera sensor to warmup,
 # and initialize the FPS counter
 print("[INFO] starting video stream...")
-vs = VideoStream(src='rtsp://admin:admin@123@192.168.1.3/Streaming/Channels/01').start()
-vs = VideoStream(src=0).start()
 
-time.sleep(2.0)
-fps = FPS().start()
+vs = VideoStream(src="C:\\Users\\Ayush\\Videos\\DJI_0012.MOV").start()
+
+# FOR LIVE STREAMING
+#vs = VideoStream(src="rtmp://10.1.1.1/live/bird").start()
+
 
 # loop over the frames from the video stream
 startTime=time.time()
 while True:
 	currentTime=time.time()
 	peopleCount=0
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
 	frame = vs.read()
+	frame=cv2.resize(frame,(1280,720))
 	#frame = imutils.resize(frame, width=400)
-
-	# grab the frame dimensions and convert it to a blob
 	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-		0.007843, (300, 300), 127.5)
 
-	# pass the blob through the network and obtain the detections and
-	# predictions
-	net.setInput(blob)
-	detections = net.forward()
-	# loop over the detections
-	for i in np.arange(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the prediction
-		confidence = detections[0, 0, i, 2]
-		
-		##		print(detections)
-		##		print(confidence)
-		
-		# filter out weak detections by ensuring the `confidence` is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# extract the index of the class label from the
-			# `detections`, then compute the (x, y)-coordinates of
-			# the bounding box for the object
-			idx = int(detections[0, 0, i, 1])
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+	if currentTime-startTime>5:
+		startTime=time.time()
 
-			# draw the prediction on the frame
-			if idx==15:
-				peopleCount+=1
-				label = "{}: {:.2f}%".format(CLASSES[0],
-					confidence * 100)
-				cv2.rectangle(frame, (startX, startY), (endX, endY),
-					COLORS[0], 2)
-				y = startY - 15 if startY - 15 > 15 else startY + 15
-				cv2.putText(frame, label, (startX, y),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[0], 2)
-				if currentTime-startTime>5:
-						startTime=time.time()
-						createLog("No of People "+str(peopleCount))
+		blob = cv2.dnn.blobFromImage(frame,0.007843, (1280, 720), 127.5)
+		net.setInput(blob)
+		detections = net.forward()
+		# loop over the detections
+		for i in np.arange(0, detections.shape[2]):
+			confidence = detections[0, 0, i, 2]
+
+			if confidence > args["confidence"]:
+				idx = int(detections[0, 0, i, 1])
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
+
+				# draw the prediction on the frame
+				if idx==15:
+					peopleCount+=1
+					label = "{}: {:.2f}%".format(CLASSES[0],
+						confidence * 100)
+					cv2.rectangle(frame, (startX, startY), (endX, endY),
+						COLORS[0], 2)
+					y = startY - 15 if startY - 15 > 15 else startY + 15
+					cv2.putText(frame, label, (startX, y),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[0], 2)
+					print(" No of people- ",peopleCount)
 
 	# show the output frame
-	cv2.imshow("Frame", frame)
+		cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
-	print(" No of people- ",peopleCount)
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
 
-	# update the FPS counter
-	fps.update()
-
-# stop the timer and display FPS information
-fps.stop()
-#print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-#print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
 # do a bit of cleanup
-cv2.destroyAllWindows()
 vs.stop()
+cv2.destroyAllWindows()
